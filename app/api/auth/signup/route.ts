6b5f4ff5db7d9e80f0de78c10d409ae349d3c13f
jonathanbarruga <jonathanbarruga@gmail.com>
2026-04-08 17:23:30 +0800

    remove pages router (fix 404)

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -47,7 +47,7 @@ export async function POST(req: Request) {
 
     const supabase = createServerClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      process.env.SUPABASE_SERVICE_ROLE_KEY!,
       {
         cookies: {
           get(name: string) {
